bd7426c5d2f55e8215df9760845a3598bf65149e
matatov.n <matatov.n@gmail.com>
2024-06-02 14:56:04 +0000

    Initial commit

---
+++ !kubeflow_practice/load_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import argparse
+from google.cloud import storage
+import os
+import io
+
+def gcp_csv_to_df(bucket_name, source_file_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_file_name)
+    data = blob.download_as_string()
+    df = pd.read_csv(io.BytesIO(data))
+    print(f'Pulled down file from bucket {bucket_name}, file name: {source_file_name}')
+    return df
+
+def read_data(data_path):
+    data = gcp_csv_to_df(data_path,'train.csv')
+    print('Original column list ->',data.columns)
+    #call simple data transform step
+    data.rename(columns = {col:col.lower() for col in data.columns},inplace = True)
+    print('Transformed column list ->',data.columns)
+
+    return None
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Data Prep code')
+    parser.add_argument('--data_path', type=str)
+    args = parser.parse_args()
+
+    #call the data prep function
+    read_data(args.data_path)
